471722229a687c6442cfd95f1a0195182f4cd8cb
mohamed21fe-tech <mohamed21fe@gmail.com>
2026-09-06 06:51:11 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const { getDict } = require('./i18n');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
+app.set('query parser', 'simple');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'public'), { maxAge: '1d' }));
